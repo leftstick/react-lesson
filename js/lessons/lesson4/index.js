4accd57dc93c5495f0b587fff81d8c05e0502adf
howard.zuo <leftstick@qq.com>
2015-12-24 21:54:17 +0800

    update missed docs

--- js/lessons/lesson4/index.js
+++ js/lessons/lesson4/index.js
@@ -32,6 +32,7 @@ class Lesson1 extends React.Component {
               <LessonTitle text='fetching users from rest API and display users in below list' />
               <LessonHelper>
                 <span>The API is: <code>http://demo1553843.mockable.io/users</code></span>
+                <DocumentLink link='http://facebook.github.io/react/tips/initial-ajax.html' text='Read initial-ajax' />
                 <DocumentLink link='https://developers.google.com/web/updates/2015/03/introduction-to-fetch' text='Read fetch API' />
               </LessonHelper>
               <Preview>
--- js/lessons/lesson5/index.js
+++ js/lessons/lesson5/index.js
@@ -19,7 +19,7 @@ class Lesson1 extends React.Component {
               <LessonTitle text='create a countdown timer' />
               <LessonHelper>
                 <span>This component should accept a number as start point, and a button to start counting it down to 0</span>
-                <DocumentLink link='http://facebook.github.io/react/tips/inline-styles.html' text='Read inline-styles' />
+                <DocumentLink link='http://facebook.github.io/react/docs/interactivity-and-dynamic-uis.html' text='Read interactivity-and-dynamic-uis' />
               </LessonHelper>
               <Preview>
                 <CountdownTimer start={ 5 } />
